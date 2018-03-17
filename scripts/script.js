0767e3ee0d160202e4eda231c746ffd01c4b4ddd
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2018-03-17 11:19:40 +0530

    searchbutton on all screens, last 3 searches stored

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,57 @@
 function init()
 {
-	function isTouchDevice() {
+	searchBar = document.getElementById("searchbar");
+	searchButton = document.getElementById("searchButton");
+	searchButton.addEventListener("click", search);
+	searchBar.addEventListener("keyup", search);
+
+	/*function isTouchDevice() {
 	    return 'ontouchstart' in document.documentElement;
 	}
 	if (isTouchDevice()) {
     	document.getElementsByClassName("searchButton")[0].style.display = 'inline-block';
+	}*/
+
+	function search(e) {
+		if(e.keyCode==13)
+		{
+			doSearch();
+		}
+		else if(this.id=="searchButton")
+		{
+			doSearch();
+		}
+		function doSearch()
+		{
+			if(searchBar.value != null && searchBar.value!=undefined && searchBar.value!="" && !isEmpty(searchBar.value))
+			{
+				queries = localStorage.getItem("queries");
+				if (queries) {
+				    queries = JSON.parse(queries);
+				    if(queries.length<3)
+				    {
+				    	queries.push(searchBar.value);
+				    }
+				    else
+				    {
+				    	queries.push(searchBar.value);
+				    	queries.splice(0,1);
+				    }
+					localStorage.setItem("queries", JSON.stringify(queries));
+				}
+				else
+				{
+					queries = new Array();
+					queries.push(searchBar.value);
+					localStorage.setItem("queries", JSON.stringify(queries));
+					queries = localStorage.getItem("queries");
+				}
+				console.log(queries);
+			}
+		}
+		function isEmpty(str){
+		    return !str.replace(/^\s+/g, '').length; // boolean (`true` if field is empty)
+		}
 	}
 }
 init();
